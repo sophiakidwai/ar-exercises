8360af832903868e90cd8d47455cf7e9c58cf203
sophia kidawai <sophia.kidwai@telus.com>
2023-01-24 21:25:11 -0800

    Exercise 1: Create 3 stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,26 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+store_burnaby = Store.create(
+  name: "Burnaby", 
+  annual_revenue: 300000, 
+  mens_apparel: true, 
+  womens_apparel: true
+)
+
+store_richmond = Store.create(
+  name: "Richmond", 
+  annual_revenue: 1260000, 
+  mens_apparel: false, 
+  womens_apparel: true
+)
+
+store_gastown = Store.create(
+  name: "Gastown", 
+  annual_revenue: 190000, 
+  mens_apparel: true, 
+  womens_apparel: false
+)
+
+puts Store.count
